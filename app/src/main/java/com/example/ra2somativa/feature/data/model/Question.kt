b07bfbc1c18200131f19bd9502d50e1e6cf21f06
abressam <amandabressam@gmail.com>
2024-06-06 00:51:00 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ra2somativa/feature/data/model/Question.kt
@@ -0,0 +1,14 @@
+package com.example.ra2somativa.feature.data.model
+
+data class Question(
+    val questionText: String,
+    val options: List<String>,
+    val correctAnswerIndex: Int,
+    val imageUrl: String
+) {
+    fun randomizeOptions(): Question {
+        val shuffledOptions = options.shuffled()
+        val newCorrectAnswerIndex = shuffledOptions.indexOf(options[correctAnswerIndex])
+        return copy(options = shuffledOptions, correctAnswerIndex = newCorrectAnswerIndex)
+    }
+}
