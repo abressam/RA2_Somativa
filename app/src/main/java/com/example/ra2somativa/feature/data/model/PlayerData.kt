836dc780ff20f2582fdd119a3fe2fdbea7a21214
abressam <amandabressam@gmail.com>
2024-07-18 23:22:17 -0300

    feat: change default players nickname

--- app/src/main/java/com/example/ra2somativa/feature/data/model/PlayerData.kt
+++ app/src/main/java/com/example/ra2somativa/feature/data/model/PlayerData.kt
@@ -4,9 +4,15 @@ class PlayerData {
     fun loadPlayers(): List<Player> = listOf(
         Player(
             id = 0,
-            nickname = "Me supere!",
+            nickname = "Mordecai",
             score = 8500,
             higherScore = 8500
+        ),
+        Player(
+            id = 0,
+            nickname = "Rigby",
+            score = 7000,
+            higherScore = 7000
         )
     )
 }
